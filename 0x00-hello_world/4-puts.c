4f587389589d0b7e460c4ee5ef6381f2265b4e3e
Louis7001 <chinedunzeogu@gmail.com>
2022-09-08 21:09:26 -0700

    Task 4-8 level C

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Printing
+ *
+ * Return: Always 0 (successful)
+ */
+int main(void)
+{
+	puts("\"Programming is like building multilingual puzzle");
+	return (0);
+}
