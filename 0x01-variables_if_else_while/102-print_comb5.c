2d7a8b07aa7fc48025cbaae85312ebf927bd5916
Louis7001 <chinedunzeogu@gmail.com>
2022-09-09 22:20:39 -0700

    alx

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -3,17 +3,12 @@
 
 /**
  *
- * main - print possible combination of two 2-digit
- *
- * numbers
- *
- *
+ * main - print possible combination of two 2-digit numbers
  *
  * Return: returns zero at the end
  *
  *
  */
-
 
 int main(void)
 
@@ -29,49 +24,29 @@ int main(void)
 			if (p > i)
 
 			{
-
-
 				putchar((i / 10) + '0');
-
 
 				putchar((i % 10) + '0');
 
-
 				putchar(' ');
-
 
 				putchar((p / 10) + '0');
 
-
 				putchar((p % 10) + '0');
-
 
 				if (i != 98)
 
-
 				{
-
-
 					putchar(',');
-
 
 					putchar(' ');
 
-
 				}
-
-
 			}
-
-
 		}
-
-
 	}
 
-
 	putchar('\n');
-
 
 	return (0);
 }
