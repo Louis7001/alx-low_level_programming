d1b25d9220e1e00cbe85a5a5354500dd1424ac6e
Louis7001 <chinedunzeogu@gmail.com>
2022-09-22 20:41:33 -0700

    alx

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * rot13 - Encodes a string using rot13.
+ * @str: the string to be encoded.
+ *
+ * Return: a pointer to the encoded string.
+ */
+char *rot13(char *str)
+{
+	int indx1 = 0, indx2;
+	char alphabt[52] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U','V','W','X', 'Y', 'Z'};
+	char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'};
+
+	while (str[indx1])
+	{
+		for (indx2 = 0; indx2 < 52; indx2++)
+		{
+			if (str[indx1] == alphabet[indx2])
+			{
+				str[indx1] = rot13key[indx2];
+				break;
+			}
+		}
+		indx1++;
+	}
+	return (str);
+}
